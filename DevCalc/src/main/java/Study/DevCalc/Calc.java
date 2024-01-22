6fe63cb433491901cdd6e5a4e20f8c3c2dd75ff8
Girfanov4 <vg757643@gmail.com>
2024-01-22 18:19:10 +0500

    Внесены изменения в класс "Calc"

--- DevCalc/src/main/java/Study/DevCalc/Calc.java
+++ DevCalc/src/main/java/Study/DevCalc/Calc.java
@@ -16,7 +16,7 @@ public class Calc extends Loan {
 	@Override
 	public double CalculateStaticPayment() {
 		double pay = loanAmount * AnnuityRate(loanTerm, interestRate);
-		pay = RoundTo(pay, 2);
+		pay = RoundTo(pay, 4);
 		return pay;
 
 	}
@@ -31,45 +31,46 @@ public class Calc extends Loan {
 
 	}
 
-	//Вычисление каждого платежа по крдиту
-		public List<Payment> CalculateDetailedPayments()
-		{
-			Map<String,String> engRus = new HashMap<String,String>();    
-		    engRus.put("january", "январь");
-		    engRus.put("february", "февраль");
-		    engRus.put("march", "март");
-		    engRus.put("april", "апрель");
-		    engRus.put("may", "май");
-		    engRus.put("june", "июнь");
-		    engRus.put("july", "июль");
-		    engRus.put("august", "август");
-		    engRus.put("september", "сентябрь");
-		    engRus.put("october", "октябрь");
-		    engRus.put("november", "ноябрь");
-		    engRus.put("december", "декабрь");
-			
-			double staticPayment = CalculateStaticPayment();
-			double loanRemain = loanAmount;
-			double monthRate = interestRate/12/100;
-			double mainPay;
-			double percents;
-			int count = 0;
-			LocalDate dateOfPayment = LocalDate.of(LocalDate.now().getYear(),LocalDate.now().getMonth(),1);
-			
-			List<Payment> payments = new ArrayList<Payment>();
-			while (loanRemain > 0)
-			{
-				count++;
-				percents = RoundTo(loanRemain*monthRate, 2);
-				mainPay = RoundTo((loanRemain + percents >= staticPayment) ? staticPayment - percents : loanRemain, 2);
-				loanRemain = RoundTo(loanRemain - mainPay, 2);
-				dateOfPayment = dateOfPayment.plusMonths(1);
-				
-				payments.add(new Payment(count, Integer.toString(dateOfPayment.getYear()) + " " + engRus.get(dateOfPayment.getMonth().toString().toLowerCase()), RoundTo(percents + mainPay, 2), mainPay, percents, loanRemain));
-			}
-			return payments;
+	// Вычисление каждого платежа по крдиту
+	public List<Payment> CalculateDetailedPayments() {
+		Map<String, String> engRus = new HashMap<String, String>();
+		engRus.put("january", "январь");
+		engRus.put("february", "февраль");
+		engRus.put("march", "март");
+		engRus.put("april", "апрель");
+		engRus.put("may", "май");
+		engRus.put("june", "июнь");
+		engRus.put("july", "июль");
+		engRus.put("august", "август");
+		engRus.put("september", "сентябрь");
+		engRus.put("october", "октябрь");
+		engRus.put("november", "ноябрь");
+		engRus.put("december", "декабрь");
+
+		double staticPayment = CalculateStaticPayment();
+		double loanRemain = loanAmount;
+		double monthRate = interestRate / 12 / 100;
+		double mainPay;
+		double percents;
+		int count = 0;
+		LocalDate dateOfPayment = LocalDate.of(LocalDate.now().getYear(), LocalDate.now().getMonth(), 1);
+
+		List<Payment> payments = new ArrayList<Payment>();
+		while (loanRemain >= 0.01) {
+			count++;
+			percents = RoundTo(loanRemain * monthRate, 4);
+			mainPay = RoundTo((loanRemain + percents >= staticPayment) ? staticPayment - percents : loanRemain, 2);
+			loanRemain = RoundTo(loanRemain - mainPay, 4);
+			dateOfPayment = dateOfPayment.plusMonths(1);
+
+			payments.add(new Payment(count,
+					Integer.toString(dateOfPayment.getYear()) + " "
+							+ engRus.get(dateOfPayment.getMonth().toString().toLowerCase()),
+					RoundTo(percents + mainPay, 2), mainPay, percents, loanRemain));
 		}
-	
+		return payments;
+	}
+
 	public static double RoundTo(double num, int scale) {
 		double sc = Math.pow(10, scale);
 		return Math.round(num * sc) / sc;
