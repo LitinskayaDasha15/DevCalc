13c10ff2c69fca063626ea1006c9c08dfaa25bc5
LitinskayaDasha15 <litinskaya.mailfortrash@gmail.com>
2024-01-23 17:08:54 +0500

    Осознана большая ошибка. Переписана форма под расчет вклада с капитализацией.

--- DevCalc/src/main/java/Study/DevCalc/MainFrame.java
+++ DevCalc/src/main/java/Study/DevCalc/MainFrame.java
@@ -10,6 +10,7 @@ import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
@@ -18,15 +19,17 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.math.MathContext;
 import java.util.List;
 
 public class MainFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JTextField loanAmount;
-	private JTextField loanTerm;
-	private JTextField annualInterestRate;
+	private JTextField depAmount;
+	private JTextField depTerm;
+	private JTextField interestRate;
 	private JComboBox<String> сomboBoxTerm;
+	private JComboBox<String> сomboBoxСapitalizationFrequency;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -60,7 +63,7 @@ public class MainFrame extends JFrame {
 		boxHead.setBorder(BorderFactory.createEmptyBorder(0, 0, 20, 0));
 		
 		//Заголовок шапки
-		JLabel labelHead1 = new JLabel("Параметры кредита");		
+		JLabel labelHead1 = new JLabel("Параметры депозита");		
 		boxHead.add(labelHead1);
 
 		
@@ -70,7 +73,7 @@ public class MainFrame extends JFrame {
 		GridBagConstraints c = new GridBagConstraints();
 		
 		//Заголовки
-		JLabel labelLoanAmount = new JLabel("Сумма кредита:");
+		JLabel labelLoanAmount = new JLabel("Сумма вклада:");
 		labelLoanAmount.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 0.0;
@@ -78,7 +81,7 @@ public class MainFrame extends JFrame {
 		c.gridy = 0;
 		pane.add(labelLoanAmount, c);
 		
-		JLabel labelLoanTerm = new JLabel("Срок кредита:");
+		JLabel labelLoanTerm = new JLabel("Срок вклада:");
 		labelLoanTerm.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
@@ -92,29 +95,43 @@ public class MainFrame extends JFrame {
 		c.gridy = 2;
 		pane.add(labelInterestRate, c);
 		
+		JLabel labelСapitalizationFrequency = new JLabel("Периодичность капитализации:");
+		labelСapitalizationFrequency.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 3;
+		pane.add(labelСapitalizationFrequency, c);
+		
 		
 		//Поля ввода	
-		loanAmount = new JTextField();
-		loanAmount.setColumns(15);
+		depAmount = new JTextField();
+		depAmount.setColumns(15);
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 0.5;
 		c.gridx = 1;
 		c.gridy = 0;
-		pane.add(loanAmount, c);
+		pane.add(depAmount, c);
 		
-		loanTerm = new JTextField();
-		loanTerm.setColumns(15);
+		depTerm = new JTextField();
+		depTerm.setColumns(15);
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 1;
 		c.gridy = 1;
-		pane.add(loanTerm, c);
+		pane.add(depTerm, c);
 		
-		annualInterestRate = new JTextField();
-		annualInterestRate.setColumns(15);
+		interestRate = new JTextField();
+		interestRate.setColumns(15);
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 1;
 		c.gridy = 2;
-		pane.add(annualInterestRate, c);
+		pane.add(interestRate, c);
+		
+		сomboBoxСapitalizationFrequency = new JComboBox<String>(new String[] {"Раз в год", "Раз в полгода", "Раз в квартал", "Раз в месяц"});
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.0;
+		c.gridx = 1;
+		c.gridy = 3;
+		pane.add(сomboBoxСapitalizationFrequency, c);
 		
 		
 		//Поля выбора
@@ -125,12 +142,11 @@ public class MainFrame extends JFrame {
 		c.gridy = 0;
 		pane.add(labelCurrencyUnit, c);
 		
-		сomboBoxTerm = new JComboBox<String>(new String[] {"лет", "мес."});
+		JLabel labelTerm = new JLabel("(в годах)");
 		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.0;
 		c.gridx = 2;
 		c.gridy = 1;
-		pane.add(сomboBoxTerm, c);
+		pane.add(labelTerm, c);
 		
 		JLabel labelRatePeriod = new JLabel("% в год");
 		c.fill = GridBagConstraints.HORIZONTAL;
@@ -138,15 +154,30 @@ public class MainFrame extends JFrame {
 		c.gridy = 2;
 		pane.add(labelRatePeriod, c);
 		
+		JLabel labelHelpCapitalization = new JLabel("[?]");
+		labelHelpCapitalization.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		labelHelpCapitalization.setToolTipText("Периодичность, с которой начисленные проценты будут добавляться к сумме вклада.");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 2;
+		c.gridy = 3;
+		pane.add(labelHelpCapitalization, c);
+		
 
 //		Подвал
 		Box boxUnder = Box.createVerticalBox();
 		
+		Component verticalStrut_1 = Box.createVerticalStrut(20);
+		boxUnder.add(verticalStrut_1);
+		
+		Box boxButtons = Box.createHorizontalBox();
+		
 		JButton btnCalculate = new JButton("Расчитать");
-		boxUnder.add(btnCalculate);
+		boxButtons.add(btnCalculate);
 		
 		JButton btnSave = new JButton("Расчитать и сохранить xls");
-		boxUnder.add(btnSave);
+		boxButtons.add(btnSave);
+		
+		boxUnder.add(boxButtons);
 		
 		final JLabel labelResult = new JLabel();
 		boxUnder.add(labelResult);
@@ -164,28 +195,7 @@ public class MainFrame extends JFrame {
 		btnCalculate.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				int amount, term;
-				double rate;
-				try
-				{
-					amount = Integer.parseInt(loanAmount.getText());
-					term = Integer.parseInt(loanTerm.getText());
-					rate = Double.parseDouble(annualInterestRate.getText());
-				}
-				catch(Exception ex)
-				{
-					labelResult.setText("Некорректный ввод");
-					labelResult.setToolTipText(ex.getMessage());
-					pack();
-					return;
-				}
-				if (сomboBoxTerm.getSelectedIndex() == 0) //0 - лет, 1 - мес.
-				{
-					term *= 12;
-				}
-				Calc pay = new Calc(amount, term, rate);
-				labelResult.setText("Ежемесячный платеж: " + Double.toString(pay.CalculateStaticPayment()) + "₽");
-				labelResult.setToolTipText("Размер платежа");
+				input(labelResult);
 				pack();
 			}
 		});
@@ -193,30 +203,11 @@ public class MainFrame extends JFrame {
 //		Событие по нажатию на кнопку btnSave		
 		btnSave.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent e) {
-				int amount, term;
-				double rate;
-				try
-				{
-					amount = Integer.parseInt(loanAmount.getText());
-					term = Integer.parseInt(loanTerm.getText());
-					rate = Double.parseDouble(annualInterestRate.getText());
-				}
-				catch(Exception ex)
-				{
-					labelResult.setText("Некорректный ввод");
-					labelResult.setToolTipText(ex.getMessage());
-					pack();
-					return;
-				}
-				if (сomboBoxTerm.getSelectedIndex() == 0) //0 - лет, 1 - мес.
-				{
-					term *= 12;
-				}
-				Calc pay = new Calc(amount, term, rate);
-				labelResult.setText("Ежемесячный платеж: " + Double.toString(pay.CalculateStaticPayment()) + "₽");
-				labelResult.setToolTipText("Размер платежа");
+			public void mouseClicked(MouseEvent e)
+			{
+				Calc pay = input(labelResult);
 				pack();
+				
 				List<Payment> pays = pay.CalculateDetailedPayments();
 				ExcelWorker excel = new ExcelWorker();
 		    	excel.FillData(pays);
@@ -226,7 +217,8 @@ public class MainFrame extends JFrame {
 		        JFileChooser fc = new JFileChooser();
 		        fc.setFileFilter(filter);
 		        fc.setAcceptAllFileFilterUsed(false);
-		        if ( fc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION ) {
+		        if ( fc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION )
+		        {
 		            try {
 		            	excel.Save(fc.getSelectedFile().toString().endsWith(".xls") ? fc.getSelectedFile().toString() : fc.getSelectedFile() + ".xls");
 		            }
@@ -234,6 +226,48 @@ public class MainFrame extends JFrame {
 		        }
 			}
 		});
+	}
+	
+	public Calc input(JLabel labelResult)
+	{
+		int amount, term, freq;
+		double rate;
+		try
+		{
+			amount = Math.abs(Integer.parseInt(depAmount.getText().replace(',', '.')));
+			term = Math.abs(Integer.parseInt(depTerm.getText().replace(',', '.')));
+			rate = Math.abs(Double.parseDouble(interestRate.getText().replace(',', '.')));
+		}
+		catch(Exception ex)
+		{
+			labelResult.setText("Некорректный ввод");
+			labelResult.setToolTipText(ex.getMessage());
+			pack();
+			return null;
+		}
+		switch (сomboBoxСapitalizationFrequency.getSelectedIndex())
+		{
+		case (0):
+			freq = 1;
+			break;
+		case (1):
+			freq = 2;
+			break;
+		case (2):
+			freq = 4;
+			break;
+		case (3):
+			freq = 12;
+			break;
+		default:
+			freq = 1;
+			break;
+		}
+		term *= freq;
+		Calc pay = new Calc(amount, term, rate, freq);
+		labelResult.setText("Сумма вклада с процентами: " + Double.toString(pay.CalculateDeposit()) + "₽");
+		labelResult.setToolTipText("Итоговая сумма по окончанию срока вклада");
+		return pay;
 	}
 
 }
