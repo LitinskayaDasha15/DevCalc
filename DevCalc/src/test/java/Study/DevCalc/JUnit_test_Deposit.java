8080cc88d1d6cc0bbd52bfb7a1fef50eb034678e
Nector102 <nectarp@mail.ru>
2024-01-25 22:02:05 +0500

    Добавлен тест абстрактного класса.

---
+++ DevCalc/src/test/java/Study/DevCalc/JUnit_test_Deposit.java
@@ -0,0 +1,14 @@
+package Study.DevCalc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JUnit_test_Deposit {
+
+	@Test
+    public void test() {
+        assertEquals("Реализовано","1","1");
+    }
+
+}
