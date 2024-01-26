3d30fd8cf5d10cd48fa3b162eb3f589d92e765e4
LitinskayaDasha15 <litinskaya.mailfortrash@gmail.com>
2024-01-26 22:13:32 +0500

    Выполнена сборка проекта.
    Собран Jar-файл.

--- DevCalc/src/test/java/laba_2/BubbleSortTest.java
+++ DevCalc/src/test/java/laba_2/BubbleSortTest.java
@@ -27,7 +27,7 @@ public class BubbleSortTest {
   public static void tearDown() {
     // Проверка, что массив отсортирован правильно
     for (int i = 0; i < array.length - 1; i++) {
-      assertTrue("Сортировка выполнена неправильно", array[i] < array[i + 1]);
+      assertTrue("Сортировка выполнена неправильно", array[i] <= array[i + 1]);
     }
 
   }
